8d697e595e87eb2bafafec65d72f73115d980482
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-08-08 20:47:53 +0000

    photos v1

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,17 @@
+class PhotosController < ActionController::Base
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:username=>:asc})
+    render({ :template => "photos_templates/index.html.erb"})
+  end
+  def show
+    url_photo = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_photo})
+    @the_photo = matching_photos.at(0)
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render({ :template => "photos_templates/show.html.erb"})
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   get("/users",{ :controller => "users",:action => "index"})
   get("/users/:username",{ :controller => "users",:action => "show"})
+
+  get("/photos",{ :controller => "photos",:action => "index"})
+  get("/photos/:path_id",{ :controller => "photos",:action => "show"})
 end
