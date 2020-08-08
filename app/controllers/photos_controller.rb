e4abdc1f15f4f303692c0e09274e421bbd426dd5
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-08-08 23:50:03 +0000

    form to add photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,4 +22,18 @@ class PhotosController < ActionController::Base
     @the_photo.destroy
     redirect_to("/photos")
   end
+
+  def create
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    input_owner_id = params.fetch("input_owner_id")
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+    a_new_photo.save
+    redirect_to("/photos/"+a_new_photo.id.to_s)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get("/photos",{ :controller => "photos",:action => "index"})
   get("/photos/:path_id",{ :controller => "photos",:action => "show"})
 
+
   get("/delete_photo/:path_id",{ :controller => "photos",:action => "delete"})
+  get("/insert_photos/",{ :controller => "photos",:action => "create"})
 end
