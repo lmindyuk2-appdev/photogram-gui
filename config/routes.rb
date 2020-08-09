1d5abf6c03944197b75560e8d6be22d1e5efee73
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-08-09 00:03:20 +0000

    end of the video

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
 
   get("/delete_photo/:path_id",{ :controller => "photos",:action => "delete"})
-  get("/insert_photos/",{ :controller => "photos",:action => "create"})
+  get("/insert_photo/",{ :controller => "photos",:action => "create"})
+  get("/update_photo/:modify_id",{ :controller => "photos",:action => "update"})
 end
